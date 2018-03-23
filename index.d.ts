6a6956244cef272200e0736de3d3affea05b038a
Shane <spp935@gmail.com>
2018-03-23 16:20:31 +1100

    improved types

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,21 @@
-import { Middleware, Dispatch, Action } from "redux";
 
+import { Middleware, Action, AnyAction } from "redux";
 
-export type ThunkAction<A extends Action, R, S, E> = (dispatch: Dispatch<A>, getState: () => S,
-  extraArgument: E) => R;
-
-declare module "redux" {
-  export interface Dispatch<A extends Action> {
-    <R, S, E>(asyncAction: ThunkAction<A, R, S, E>): R;
-  }
+export interface ThunkDispatch<S, E, A extends Action> {
+  <T extends A>(action: T): T;
+  <R>(asyncAction: ThunkAction<R, S, E, A>): R;
 }
 
+export type ThunkAction<R, S, E, A extends Action> = (
+  dispatch: ThunkDispatch<S, E, A>,
+  getState: () => S,
+  extraArgument: E
+) => R;
 
-declare const thunk: Middleware & {
-  withExtraArgument(extraArgument: any): Middleware;
-};
+export type ThunkMiddleware<S = {}, A extends Action = AnyAction, E = undefined> = Middleware<ThunkDispatch<S, E, A>, S, ThunkDispatch<S, E, A>>;
+
+declare const thunk: ThunkMiddleware & {
+  withExtraArgument<E>(extraArgument: E): ThunkMiddleware<{}, AnyAction, E>
+}
 
 export default thunk;
